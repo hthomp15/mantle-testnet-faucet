ec5c0c3d6ceb89f54268d9b2f9c3f3454033a99e
hthomp15 <hailethomp@gmail.com>
2025-02-04 15:18:31 -0500

    hard coding cord urls, not working with railway variables for some reason

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,11 @@ const port = process.env.PORT || 5001;
 // Middleware
 app.use(express.json());
 
-const DEVELOPMENT_URL = process.env.DEVELOPMENT_URL;
-const PRODUCTION_URL = process.env.PRODUCTION_URL;
-
-// Determine the allowed origin based on the environment
-const allowedOrigins = process.env.NODE_ENV === 'production' ? [PRODUCTION_URL] : [DEVELOPMENT_URL];
-
 app.use(cors({
-  origin: allowedOrigins,
+  origin: [
+    'https://mantle-testnet-faucet.vercel.app',
+    'http://localhost:3000'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
   credentials: true // Allow credentials if needed
 }));
